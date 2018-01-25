82f7ec9276deddc43d3dc72d566bc912cad47a68
lukazikus <cnylucas_95@hotmail.com>
2018-01-25 10:42:14 -0500

    Added function reference and new topic name

--- src/contest1.cpp
+++ src/contest1.cpp
@@ -48,7 +48,7 @@ void bumperCallback(const kobuki_msgs::BumperEvent msg){
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 	laserSize = (msg->angle_max - msg->angle_min)/msg->angle_increment; // Number of range indices in sensor field of view
 	laserOffset = desiredAngle*pi/(180*msg->angle_increment); // Number of range indices in desired field of view
-	laserRange = 11; // Minimum number of range indices
+	laserRange = 11; // Minimum distance in metres
 
 	if(desiredAngle*pi/180 < msg->angle_max && -desiredAngle*pi/180 > msg->angle_min){ // Desired FOV is smaller than sensor FOV
 		for(int i = laserSize/2 - laserOffset; i < laserSize/2 + laserOffset; i++){
@@ -85,9 +85,9 @@ int main(int argc, char **argv)
 	ros::NodeHandle nh;
 	teleController eStop;
 
-	ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
-	ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
-	ros::Subscriber odom = nh.subscribe("/odom", 1, odomCallback);
+	ros::Subscriber bumper_sub = nh.subscribe("/mobile_base/events/bumper", 10, &bumperCallback);
+	ros::Subscriber laser_sub = nh.subscribe("/scan", 10, &laserCallback);
+	ros::Subscriber odom = nh.subscribe("/odom", 1, &odomCallback);
 
 	vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
