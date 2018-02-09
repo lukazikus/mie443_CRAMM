585812a4c900491bc26465682ba08dd39c038c56
= <christopher.lucasius@gmail.com>
2018-02-09 17:37:22 -0500

    simplified code

--- src/contest1.cpp
+++ src/contest1.cpp
@@ -17,6 +17,7 @@
 
 #include <nav_msgs/Odometry.h>
 #include <tf/transform_datatypes.h>
+#include <string>
 
 using namespace std;
 ros::Publisher vel_pub;
@@ -34,6 +35,15 @@ bool bumperLeft = 0, bumperCenter = 0, bumperRight = 0;
 //laser variables
 double laserRange = 10;
 int laserSize = 0, laserOffset = 0, desiredAngle = 10;
+double dist = 11;
+double yawStart = 0;
+double d = 0.52;
+
+//robot direction
+int direction = 0;
+float rot = 0;
+float RES_ANG = 0.1;
+
 
 void bumperCallback(const kobuki_msgs::BumperEvent msg){
 	if(msg.bumper == 0){
@@ -45,30 +55,25 @@ void bumperCallback(const kobuki_msgs::BumperEvent msg){
 	}
 }
 
-void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
+void scanProfile (const sensor_msgs::LaserScan::ConstPtr& msg){ 
+	int i;
+	double min_angle = msg->angle_min + pi/2;	
 	laserSize = (msg->angle_max - msg->angle_min)/msg->angle_increment; // Number of range indices in sensor field of view
-	laserOffset = desiredAngle*pi/(180*msg->angle_increment); // Number of range indices in desired field of view
-	laserRange = 11; // Minimum distance in metres
-
-	if(desiredAngle*pi/180 < msg->angle_max && -desiredAngle*pi/180 > msg->angle_min){ // Desired FOV is smaller than sensor FOV
-		for(int i = laserSize/2 - laserOffset; i < laserSize/2 + laserOffset; i++){
-			if(laserRange > msg->ranges[i]){
-				laserRange = msg->ranges[i];
+	dist = 11;
+	
+	for (i = 0; i < laserSize; i++) { // loop through each laser scan index
+		if(abs(msg->ranges[i] / tan(min_angle + i*msg->angle_increment)) <= d ){
+			// laser ranges are within the left square
+			if(msg->ranges[i] < dist) {
+				dist = msg->ranges[i];
 			}
 		}
-	}else{ // Desired FOV is larger than sensor FOV
-		for(int i = 0; i < laserSize; i++){
-			if(laserRange > msg->ranges[i]){
-				laserRange = msg->ranges[i];
-			}
-		}
+	
 	}
 
-	if(laserRange == 11){
-		laserRange = 0;
+	if(dist == 11){
+		dist = 0;
 	}
-
-	ROS_INFO("Size of laser scan array: %i and size offset: %i", laserSize, laserOffset);
 }
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg){
@@ -76,62 +81,156 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg){
 	posY = msg->pose.pose.position.y;
 	yaw = tf::getYaw(msg->pose.pose.orientation);
 
-	ROS_INFO("Position: (%f, %f) Orientation: %f rad or %f degrees.", posX, posY, yaw, yaw*180/pi);
+	ROS_INFO("Position: (%f, %f) Orientation: %f rad or %f degrees.\n", posX, posY, yaw, yaw*180/pi);
 }
 
 int main(int argc, char **argv)
-{
+{	
 	ros::init(argc, argv, "image_listener");
 	ros::NodeHandle nh;
 	teleController eStop;
-
-	ros::Subscriber bumper_sub = nh.subscribe("/mobile_base/events/bumper", 10, &bumperCallback);
-	ros::Subscriber laser_sub = nh.subscribe("/scan", 10, &laserCallback);
-	ros::Subscriber odom = nh.subscribe("/odom", 1, &odomCallback);
+	
+	ros::Subscriber laser_sub = nh.subscribe("/scan", 10, &scanProfile);
+	ros::Subscriber odom = nh.subscribe("/odom", 1, odomCallback);
 
 	vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
-
 	angular = 0.0;
 	linear = 0.0;
 	geometry_msgs::Twist vel;
-
-	while(ros::ok()){
+	
+	while(ros::ok){
 		ros::spinOnce();
 		//.....**E-STOP DO NOT TOUCH**.......
 		eStop.block();
-		//...................................
-
-		//fill with your code
-		ROS_INFO("Position: (%f, %f) Orientation: %f degrees Range: %f", posX, posY, yaw*180/pi, laserRange);
-
-		//Odometry and bumper code
-		if(posX < 0.5 && yaw < pi/2 && !bumperRight && !bumperCenter && !bumperLeft && laserRange > 0.7){
-			angular = 0.0;
+		if(dist > 0.6){
+			//drive forward
+			printf("fwd\n");
 			linear = 0.2;
-		}else if(yaw < pi/2 && posX > 0.5 && !bumperRight && !bumperCenter && !bumperLeft && laserRange > 0.5){
-			angular = pi/6;
-			linear = 0.0;
-		}else if(laserRange > 1.0 && !bumperRight && !bumperCenter && !bumperLeft){
-			if(yaw < 17*pi/36 || posX > 0.6){
-				angular = pi/12;
-				linear = 0.1;
-			}else if(yaw > 19*pi/36 || posX < 0.4){
-				angular = -pi/12;
-				linear = 0.1;
-			}else{
-				angular = 0.0;
-				linear =  0.1;
-			}
-		}else{
 			angular = 0.0;
-			linear = 0.0;
+			vel.linear.x = linear;
+			vel.angular.z = angular;
+			vel_pub.publish(vel);
 		}
-
-		vel.angular.z = angular;
-		vel.linear.x = linear;
-
-		vel_pub.publish(vel);
+		else{
+			//stop, pause, turn
+			printf("turn\n");
+			linear = 0.0;
+			vel.linear.x = linear;
+			vel_pub.publish(vel);
+			sleep(1);
+			
+			direction = rand() % 3; //random number 0 or 1
+			printf("DIRECTION:%d \n",direction);
+			// Detect if the robot needs to change directions
+			if(direction == 0){ // Make robot face left
+				printf("1111111111111111\n");
+				// Change directions
+				while(ros::ok() && abs(abs(yaw)-pi) > RES_ANG){
+					// printf("CURRENT YAW: %f, DESIRED YAW: %f\n", yaw, pi);
+					ros::spinOnce();
+					linear = 0.0;
+					rot = yaw > 0 && yaw < pi? 1:-1; // Rotate in the direction of shortest angular displacement
+					angular = pi/6*rot;
+					vel.angular.z = angular;
+					vel.linear.x = linear;
+					vel_pub.publish(vel);
+					ros::spinOnce();
+					// usleep(5000);
+				}
+			}else if(direction == 1){ // Make robot face right
+				printf("222222222222222\n");
+				printf("hello it's me yaw %f \n", yaw);
+				while(ros::ok() && abs(yaw) > RES_ANG){
+					ros::spinOnce();
+					printf("Stuck facing right\n");
+					linear = 0.0;
+					rot =  yaw > -pi && yaw < 0 ? 1:-1;
+					angular = pi/6*rot;
+					vel.angular.z = angular;
+					vel.linear.x = linear;
+					vel_pub.publish(vel);
+					ros::spinOnce();
+					// usleep(5000);
+				}
+			}else if(direction == 2){ // Make robot face down
+				printf("3333333333333\n");
+				while(ros::ok() && abs(yaw + pi/2) > RES_ANG){
+					ros::spinOnce();
+					printf("Stuck facing down\n");
+					linear = 0.0;
+					rot = (yaw > -pi && yaw < -pi/2) || (yaw > pi/2 && yaw < pi) ? 1:-1;
+					angular = pi/6*rot;
+					vel.angular.z = angular;
+					vel.linear.x = linear;
+					vel_pub.publish(vel);
+					ros::spinOnce();
+					// usleep(5000);
+				}
+			}else if (direction == 3){// Make robot face up
+				printf("444444444444\n");
+				while(ros::ok() && abs(yaw - pi/2) > RES_ANG){
+					ros::spinOnce();
+					// printf("Stuck facing up\n");
+					printf("CURRENT YAW: %f, DESIRED YAW: %f\n", yaw, pi/2);
+					linear = 0.0;
+					rot = yaw > -pi/2 && yaw < pi/2 ? 1:-1;
+					angular = pi/6*rot;
+					printf("Angular velocity: %f\n", angular);
+					vel.angular.z = angular;
+					vel.linear.x = linear;
+					vel_pub.publish(vel);
+					ros::spinOnce();
+					// usleep(5000);
+				}
+			}
+			// if(direction){
+			// 	//rotate right counterclockwise
+			// 	yawStart = yaw;
+			// 	printf("left yaw, yaw desired: (%f, %f) \n",abs(yaw + pi),abs(yawStart + pi - pi/2));
+			// 	while(ros::ok() && abs(yaw + pi) < abs(yawStart + pi + pi/2) ){
+			// 		printf("right yaw, yaw desired: (%f, %f) \n",abs(yaw + pi),abs(yawStart + pi - pi/2));
+			// 		ros::spinOnce();
+			// 		linear = 0.0;
+			// 		angular = -pi/6;
+			// 		vel.angular.z = angular;
+			// 		vel.linear.x = linear;
+			// 		vel_pub.publish(vel);
+			// 	}
+			// 	//stop the robot
+			// 	ros::spinOnce();
+			// 	linear = 0.0;
+			// 	angular = 0.0;
+			// 	vel.angular.z = angular;
+			// 	vel.linear.x = linear;
+			// 	vel_pub.publish(vel);
+			// }
+			// else{
+			// 	//rotate left (assuming -pi/2 is counterclockwise)
+			// 	yawStart = yaw;
+			// 	//printf("Yaw Value ELSE %f \n",yaw);
+			// 	printf("left yaw, yaw desired: (%f, %f) \n",abs(yaw),abs(yawStart - pi/2));
+			// 	while(ros::ok() && abs(yaw) > abs(yawStart- pi/2) ){
+			// 		printf("left yaw, yaw desired: (%f, %f) \n",abs(yaw,abs(yawStart + pi - pi/2));
+					
+			// 		//printf("turn left\n");
+			// 		ros::spinOnce();
+			// 		linear = 0.0;
+			// 		angular = pi/6;
+			// 		vel.angular.z = angular;
+			// 		vel.linear.x = linear;
+			// 		vel_pub.publish(vel);
+			// 	}
+			// 	//stop the robot
+			// 	ros::spinOnce();
+			// 	linear = 0.0;
+			// 	angular = 0.0;
+			// 	vel.angular.z = angular;
+			// 	vel.linear.x = linear;
+			// 	vel_pub.publish(vel);
+			// }
+		}
+		
 	}
-
 	return 0;
 }
+
